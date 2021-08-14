efaf4267947e3088073e5c3dc0c2ab54c50f9378
seuribe <seuribe@bancolombia.com.co>
2021-08-14 13:55:18 -0500

    servicio de busqueda de secuencias

--- src/main/java/co/com/mercadolibre/xmen/service/implementation/SearchSequenceService.java
+++ src/main/java/co/com/mercadolibre/xmen/service/implementation/SearchSequenceService.java
@@ -3,23 +3,124 @@ package co.com.mercadolibre.xmen.service.implementation;
 import co.com.mercadolibre.xmen.service.ISearchSequence;
 
 public class SearchSequenceService implements ISearchSequence {
+
     @Override
     public int getHorizontal(String[] dna) {
-        return 0;
+        int n = dna.length;
+        int sequences = 0;
+        int countRow = 1;
+        for (int i = 0; i < n; i++) {
+            char character = dna[i].charAt(0);
+            for (int j = 0; j < n - 1; j++) {
+                char nextCharacter = dna[i].charAt(j + 1);
+                if (character == nextCharacter) {
+                    countRow++;
+                } else {
+                    if (countRow >= 4) sequences++;
+                    countRow = 1;
+                    character = nextCharacter;
+                }
+            }
+            if (countRow >= 4) sequences++;
+            countRow = 1;
+        }
+        return sequences;
     }
 
     @Override
     public int getVertical(String[] dna) {
-        return 0;
+        int n = dna.length;
+        int sequences = 0;
+        int countRow = 1;
+        for (int i = 0; i < n; i++) {
+            char character = dna[0].charAt(i);
+            for (int j = 0; j < n - 1; j++) {
+                char nextCharacter = dna[j + 1].charAt(i);
+                if (character == nextCharacter) {
+                    countRow++;
+                } else {
+                    if (countRow >= 4) sequences++;
+                    countRow = 1;
+                    character = nextCharacter;
+                }
+            }
+            if (countRow >= 4) sequences++;
+            countRow = 1;
+        }
+        return sequences;
     }
 
     @Override
     public int getDiagonal(String[] dna) {
-        return 0;
+        int n = dna.length;
+        int i = n - 4;
+        int j = 0;
+        int k = 4;
+        int countRow = 1;
+        int sequences = 0;
+        while (i >= 0 && j <= n - 4) {
+            int ii = i;
+            int jj = j;
+            char character = dna[ii].charAt(jj);
+            for (int l = 0; l < k-1; l++) {
+                char nextCharacter = dna[ii+1].charAt(jj+1);
+                if (character == nextCharacter) {
+                    countRow++;
+                } else {
+                    if (countRow >= 4) sequences++;
+                    countRow = 1;
+                    character = nextCharacter;
+                }
+                ii++;
+                jj++;
+            }
+            if (countRow >= 4) sequences++;
+            countRow = 1;
+            if (i == 0) {
+                j++;
+                k--;
+            } else {
+                i--;
+                k++;
+            }
+        }
+        return sequences;
     }
 
     @Override
     public int getInverseDiagonal(String[] dna) {
-        return 0;
+        int n = dna.length;
+        int i = n - 4;
+        int j = n-1;
+        int k = 4;
+        int countRow = 1;
+        int sequences = 0;
+        while (i >= 0 && j >= n - 4) {
+            int ii = i;
+            int jj = j;
+            char character = dna[ii].charAt(jj);
+            for (int l = 0; l < k-1; l++) {
+                char nextCharacter = dna[ii+1].charAt(jj-1);
+                if (character == nextCharacter) {
+                    countRow++;
+                } else {
+                    if (countRow >= 4) sequences++;
+                    countRow = 1;
+                    character = nextCharacter;
+                }
+                ii++;
+                jj--;
+            }
+            if (countRow >= 4) sequences++;
+            countRow = 1;
+            if (i == 0) {
+                j--;
+                k--;
+            } else {
+                i--;
+                k++;
+            }
+        }
+        return sequences;
     }
 }
---
+++ src/test/java/co/com/mercadolibre/xmen/service/implementation/SearchSequenceServiceTest.java
@@ -0,0 +1,89 @@
+package co.com.mercadolibre.xmen.service.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes = SearchSequenceService.class)
+public class SearchSequenceServiceTest {
+
+    @InjectMocks
+    SearchSequenceService searchSequenceService;
+
+    @Test
+    public void zeroSequencesHorizontalTest() {
+        int response = searchSequenceService.getHorizontal(new String[]{"ATCC","GATA","CGAG","TCGC"});
+        assertEquals(0, response);
+    }
+
+    @Test
+    public void twoSequencesHorizontalTest() {
+        int response = searchSequenceService.getHorizontal(new String[]{"ATCCATG","TTTTAGC","TTCCGAG","TGCCCCA","GGGATCG","TAATCTC","TGCTAGC"});
+        assertEquals(2, response);
+    }
+
+    @Test
+    public void threeSequencesHorizontalTest() {
+        int response = searchSequenceService.getHorizontal(new String[]{"GATCCATG","GATTTTTC","AACTTCCG","ATGCCCCA","TCGTAAAA","GTAATCTC","GTGCTAGC","ATACTAGC"});
+        assertEquals(3, response);
+    }
+
+    @Test
+    public void zeroSequencesVerticalTest() {
+        int response = searchSequenceService.getVertical(new String[]{"ATCC","GATA","CGAG","TCGC"});
+        assertEquals(0, response);
+    }
+
+    @Test
+    public void twoSequencesVerticalTest() {
+        int response = searchSequenceService.getVertical(new String[]{"ATCCATG","TGCTAGC","TTCCGAG","TGCATCA","TGGATCG","TAATCTC","TGCTAGC"});
+        assertEquals(2, response);
+    }
+
+    @Test
+    public void threeSequencesVerticalTest() {
+        int response = searchSequenceService.getVertical(new String[]{"ATCCATGG","AGCTAGCC","GTACGAGG","CGCATCTA","TGCATCTC","AACTCTTC","GGCTAGTC","ATCCATGC"});
+        assertEquals(3, response);
+    }
+
+    @Test
+    public void zeroSequencesDiagonalTest() {
+        int response = searchSequenceService.getDiagonal(new String[]{"ATCC","GATA","CGAG","TCGC"});
+        assertEquals(0, response);
+    }
+
+    @Test
+    public void twoSequencesDiagonalTest() {
+        int response = searchSequenceService.getDiagonal(new String[]{"ATCAATG","TGTTAGC","TTTCGAG","AGCTTCA","TGGATCG","AAATCTC","TGCTAGT"});
+        assertEquals(2, response);
+    }
+
+    @Test
+    public void threeSequencesDiagonalTest() {
+        int response = searchSequenceService.getDiagonal(new String[]{"GCCAATGG","AGCTAGCC","GTGCGAGG","CGCATCAA","TGTATCTA","AACTCTTC","GGCTTGTC","ATCCATGT"});
+        assertEquals(3, response);
+    }
+
+    @Test
+    public void zeroSequencesInverseDiagonalTest() {
+        int response = searchSequenceService.getInverseDiagonal(new String[]{"ATCC","GATA","CGAG","TCGC"});
+        assertEquals(0, response);
+    }
+
+    @Test
+    public void twoSequencesInverseDiagonalTest() {
+        int response = searchSequenceService.getInverseDiagonal(new String[]{"ATCAATG","TGTTAGC","TTTGGAC","AGGTTCA","TGGACCG","GAACCAC","TGCTAGC"});
+        assertEquals(2, response);
+    }
+
+    @Test
+    public void threeSequencesInverseDiagonalTest() {
+        int response = searchSequenceService.getInverseDiagonal(new String[]{"GCCAGTGG","AGCGAGTC","GTGCGTGG","CGCATCCA","TGTGACTA","AAGACTTC","GGCTTGTC","GTCTATGT"});
+        assertEquals(3, response);
+    }
+}
